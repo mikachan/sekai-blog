f1cb4af0848c855893fd29d5ed78d9e7aba82cbe
Sarah Norris <sarah@sekai.co.uk>
2021-03-31 23:01:44 +0100

    chore: purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: [
+		'./src/**/*.js',
+		'./src/**/*.jsx',
+		'./src/**/*.ts',
+		'./src/**/*.tsx',
+	],
+	theme: {},
+	variants: {},
+	plugins: [],
+};
